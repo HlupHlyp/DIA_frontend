a5ee5dc956ac889e08343448a52e6dbc6d194481
HlupHlyp <Dru0998@yandex.ru>
2024-11-26 14:02:37 +0000

    WO Navbar

--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -1,18 +1,37 @@
-import { FC } from 'react'
-import { Button } from 'react-bootstrap'
-import './Header.css'
-import card_img from "../../assets/card.png";
-import { Card } from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import LogoImage from "../../assets/Logo.jpg";
+import './Header.css';
+import { FC } from 'react';
 
 interface Props {
     onSubmit: () => void
 }
 
 export const Header: FC<Props> = ({ onSubmit }) => (
-    <div className="header">
-        <div onClick={onSubmit} className="image_box">
-            <img src={LogoImage} className="image" />
-        </div>
-    </div>
+    <Navbar bg="light" expand="lg" className="header">
+        <Container className="header">
+            <Navbar.Brand href="/" className="image_box"><img src={LogoImage} className="image" /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">
+                            Another action
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action/3.4">
+                            Separated link
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
 )
+
